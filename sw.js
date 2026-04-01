8386843ce81bf72dcb1960f912fe1046ff27df06
toreypellegrini-commits <torey.pellegrini@gmail.com>
2026-04-01 15:47:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Service Worker v1
 // Bump the version string below every time you push a new build
 // to ensure all users get the update automatically.
-const VERSION = 'dialed-v9';
+const VERSION = 'dialed-v10';
 const CACHE_NAME = VERSION;
 
 // Files to cache for offline use
